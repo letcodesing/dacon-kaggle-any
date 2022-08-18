46e6d3700e174400de622bec2e53bb14e16f29ae
letcodesing <103926252+letcodesing@users.noreply.github.com>
2022-08-18 11:43:59 +0900

    po

--- chung/chung_202_model.py
+++ chung/chung_202_model.py
@@ -1,39 +1,81 @@
-import numpy as np
+
 import pandas as pd
-path = 'C:/Users/aia200/OneDrive - 한국방송통신대학교/beat/colab testin/dacon-kaggle-any/chung/npy/'
+import numpy as np
+import glob
 
-x = np.load(path+'case01_x.npy',allow_pickle=True)
-y = np.load(path+'case01_y.npy',allow_pickle=True)
+path = 'https://we.tl/t-PXNbvaTt8m/'
+all_input_list = sorted(glob.glob(path + 'train_input/*.csv'))
+all_target_list = sorted(glob.glob(path + 'train_target/*.csv'))
 
-print(x.shape, y.shape)
-from keras.models import Sequential
-from keras.layers import Dense,Dropout
+train_input_list = all_input_list[:50]
+train_target_list = all_target_list[:50]
 
-# x = x.fillna(0)
-def split_x(dataset, size):
-    aaa = []
-    for i in range(29):
-        subset = dataset[i*size:i*size+size+2]
-        aaa.append(subset)
-    print(type(aaa))
-    return np.array(aaa)
+val_input_list = all_input_list[50:]
+val_target_list = all_target_list[50:]
 
-x = split_x(x,1439)
-print(x.shape)
-print(x[0])
-print(x[1])
-print(x[2])
-print(x[3])
-print(x[4])
-print(x[5])
-print(x[6])
-print(x[7])
-# 1~1442 1443~2884 2884~4002
+# print(all_input_list)
+print(val_input_list)
+print(len(val_input_list))  # 8
+
+def aaa(input_paths, target_paths): #, infer_mode):
+    input_paths = input_paths
+    target_paths = target_paths
+    # self.infer_mode = infer_mode
+   
+    data_list = []
+    label_list = []
+    print('시작...')
+    # for input_path, target_path in tqdm(zip(input_paths, target_paths)):
+    for input_path, target_path in zip(input_paths, target_paths):
+        input_df = pd.read_csv(input_path)
+        target_df = pd.read_csv(target_path)
+       
+        input_df = input_df.drop(columns=['시간'])
+        input_df = input_df.fillna(0)
+       
+        input_length = int(len(input_df)/1440)
+        target_length = int(len(target_df))
+        print(input_length, target_length)
+       
+        for idx in range(target_length):
+            time_series = input_df[1440*idx:1440*(idx+1)].values
+            # self.data_list.append(torch.Tensor(time_series))
+            data_list.append(time_series)
+        for label in target_df["rate"]:
+            label_list.append(label)
+    return np.array(data_list), np.array(label_list)
+    print('끗.')
+
+train_data, label_data = aaa(train_input_list, train_target_list) #, False)
+
+print(train_data[0])
+print(len(train_data), len(label_data)) # 1607 1607
+print(len(train_data[0]))   # 1440
+print(label_data)   # 1440
+print(train_data.shape, label_data.shape)   # (1607, 1440, 37) (1607,)
 
 
 
 
+# train_data = train_data.reshape(-1,train_data.shape[1]*train_data.shape[2])
+print(train_data.shape, label_data.shape)   # (1607, 1440, 37) (1607,)
 
+from keras.models import Model, load_model
+from keras.layers import Input, Conv1D, Flatten,Reshape, Conv2D,LSTM
+from sklearn.ensemble import RandomForestRegressor
+# model = RandomForestRegressor()
+in1 = Input(shape=(1440,37))
+in1 = Conv1D(64,2)(in1)
+in1 = Flatten()(in1)
+d1 = Dense(1)(in1)
+model = Model(inputs=in1,outputs=d1)
+model.summary()
+model.compile(loss='rmse',optimizer='adam')
+model.fit(train_data,label_data,epochs=10)
+test_input_list = sorted(glob.glob('C:/Users/aiapalm/OneDrive - KNOU/beat/colab testin/dacon-kaggle-any/chung/test_input/*.csv'))
+test_target_list = sorted(glob.glob('C:/Users/aiapalm/OneDrive - KNOU/beat/colab testin/dacon-kaggle-any/chung/test_target/*.csv'))
 
-model = Sequential()
-model.add(Dense(20))
+test, submit = aaa(test_input_list, test_target_list) #, False)
+pred = model.predict(test)
+print(submit)
+print(pred)
